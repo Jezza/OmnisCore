d8838fe4ed8bcaa89bdf93f9913a4e3e7cee35ec
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-10-28 22:49:14 +1100

    Refactor

--- dc_common/me/jezza/dc/client/gui/components/interactions/GuiSimpleTexturedButton.java
+++
@@ -1,9 +0,0 @@
-package me.jezza.dc.client.gui.components.interactions;
-
-import me.jezza.dc.client.gui.components.GuiTexturedButton;
-
-public class GuiSimpleTexturedButton extends GuiTexturedButton<GuiSimpleTexturedButton> {
-    public GuiSimpleTexturedButton(int x, int y, int u, int v, int width, int height) {
-        super(x, y, u, v, width, height);
-    }
-}
