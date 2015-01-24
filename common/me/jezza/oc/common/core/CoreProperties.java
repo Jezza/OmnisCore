05dc24e2e01c176c8283ef45345db6a497144bbc
NPException <npexception42@gmail.com>
2015-01-24 15:11:36 +0100

    version change

--- common/me/jezza/oc/common/core/CoreProperties.java
+++ common/me/jezza/oc/common/core/CoreProperties.java
@@ -6,9 +6,9 @@ public class CoreProperties {
 
     public static final String MOD_ID = "OmnisCore";
     public static final String MOD_NAME = "Omnis Core";
-    public static final String VERSION = "0.0.01";
+    public static final String VERSION = "0.0.02";
 
-    public static final String API_VERSION = "0.0.01";
+    public static final String API_VERSION = "0.0.02";
 
     private static final String BUILD = "1217";
 
